a00c56a0d527c24866e47202def44972f18f5b67
Rebecca Goveia <rgoveia@asu.edu>
2016-08-18 01:06:57 -0700

    Extracted the titles from 3 websites

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 import urllib
+import re
 
-htmlfile = urllib.urlopen("http://google.com")
-htmltext = htmlfile.read()
-print htmltext
+urls = ["http://google.com", "http://nytimes.com", "http://www.cnn.com"]
+i = 0
+regex = '<title>(.+?)</title>'
+pattern = re.compile(regex)
+
+while i < len(urls):
+   htmlfile = urllib.urlopen(urls[i])
+   htmltext = htmlfile.read()
+   titles = re.findall(pattern, htmltext)
+   print titles
+   #print htmltext[0:100]
+   i += 1
